501af19f5cb6f341132255686922844f1398bdbd
Khalifa <alikhalifa1911@gmail.com>
2025-03-21 16:30:32 +0200

    testing new changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { rules } from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,6 +17,11 @@ const eslintConfig = [
     "plugin:prettier/recommended",
   ),
   ...compat.config({ ignorePatterns: ["**/canvas-smoke-effect.ts"] }),
+  {
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
