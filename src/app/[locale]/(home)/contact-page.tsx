8d4d2efbcf09e1cfd9fbd9c1e05759bfe74b8280
Khalifa <alikhalifa1911@gmail.com>
2025-03-31 00:50:20 +0200

    depolying contact page

---
+++ src/app/[locale]/(home)/contact-page.tsx
@@ -0,0 +1,14 @@
+import ContactForm from "./components/contact-form";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import "./home.css";
+
+export default function Contact() {
+  return (
+    <>
+      <Header />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
