e436d09260f44d6b0336bab8a879bc211b373463
abdotaker608 <abdotaker607@gmail.com>
2025-04-05 21:41:49 +0200

    Add German language

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const i18nLocales = [
   { label: "English", code: "en", flag: "us" },
   { label: "Arabic", code: "ar", flag: "eg" },
+  { label: "German", code: "de", flag: "de" },
 ];
 
 export const routing = defineRouting({
