05b23cfc2e0c890a83772050916cef7888279252
abdotaker608 <abdotaker607@gmail.com>
2025-04-05 20:28:05 +0200

    Fix static rendering issue

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -4,12 +4,23 @@ import Services from "./components/services";
 import About from "./components/about";
 import Footer from "./components/footer";
 import "./home.css";
+import { routing } from "@/i18n/routing";
+import { use } from "react";
+import { setRequestLocale } from "next-intl/server";
 
 export async function generateStaticParams() {
-  return [{ locale: "en" }];
+  return routing.locales.map((locale) => ({ locale }));
 }
 
-export default function Home() {
+export default function Home({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = use(params);
+
+  setRequestLocale(locale);
+
   return (
     <>
       <Header />
--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,13 +1,24 @@
+import { routing } from "@/i18n/routing";
 import ContactForm from "../(home)/components/contact-form";
 import Footer from "../(home)/components/footer";
 import Header from "../(home)/components/header";
 import "../(home)/home.css";
+import { use } from "react";
+import { setRequestLocale } from "next-intl/server";
 
 export async function generateStaticParams() {
-  return [{ locale: "en" }];
+  return routing.locales.map((locale) => ({ locale }));
 }
 
-export default function Contact() {
+export default function Contact({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = use(params);
+
+  setRequestLocale(locale);
+
   return (
     <>
       <Header />
