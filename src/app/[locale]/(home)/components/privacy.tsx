b56740a4f3208ba4976e3e6b21d9ae23a574e5e3
Khalifa <alikhalifa1911@gmail.com>
2025-04-27 03:59:17 +0300

    translating privacy notice to AR & DE

--- src/app/[locale]/(home)/components/privacy.tsx
+++ src/app/[locale]/(home)/components/privacy.tsx
@@ -4,8 +4,8 @@ const PrivacyNotice = () => {
   const t = useTranslations("PrivacyPage");
 
   return (
-    <article>
-      <section id="privacy" className="about-background">
+    <article className="about-background">
+      <section id="privacy">
         <div className="wrapper flow">
           <h1 className="title">{t("privacy-header")}</h1>
           <div className="orange-border-bottom" data-width="medium"></div>
@@ -23,6 +23,46 @@ const PrivacyNotice = () => {
             <p>{t("privacy-data-desc-2")}</p>
             <p>{t("privacy-data-desc-3")}</p>
             <p>{t("privacy-data-desc-4")}</p>
+          </div>
+          <div className="spacing">
+            <h2 className="sec-title">{t("privacy-data-processing")}</h2>
+            <p>{t("privacy-data-collect-desc")}</p>
+            <h3>{t("privacy-data-collect-l1-1")}</h3>
+            <ul>
+              <li>{t("privacy-data-collect-l1-2")}</li>
+              <li>{t("privacy-data-collect-l1-3")}</li>
+              <li>{t("privacy-data-collect-l1-4")}</li>
+              <li>{t("privacy-data-collect-l1-5")}</li>
+            </ul>
+            <p>{t("privacy-data-not-collect-desc")}</p>
+            <ul>
+              <li>{t("privacy-data-not-collect-l1-1")}</li>
+              <li>{t("privacy-data-not-collect-l1-2")}</li>
+              <li>{t("privacy-data-not-collect-l1-3")}</li>
+            </ul>
+          </div>
+          <div className="spacing">
+            <h3>{t("privacy-process-data")}</h3>
+            <p>{t("privacy-process-data-desc")}</p>
+            <ul>
+              <li>{t("privacy-process-data-li-1")}</li>
+              <li>{t("privacy-process-data-li-2")}</li>
+              <li>{t("privacy-process-data-li-3")}</li>
+            </ul>
+          </div>
+          <div className="spacing">
+            <h3>{t("privacy-data-sharing")}</h3>
+            <p>{t("privacy-data-sharing-desc")}</p>
+            <ul>
+              <li>{t("privacy-data-sharing-li-1")}</li>
+            </ul>
+          </div>
+          <div className="spacing">
+            <h3>{t("privacy-data-retention")}</h3>
+            <ul>
+              <li>{t("privacy-data-retention-li-1")}</li>
+              <li>{t("privacy-data-retention-li-2")}</li>
+            </ul>
           </div>
         </div>
       </section>
