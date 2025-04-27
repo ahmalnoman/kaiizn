dcc7156baed82c4834fa99e43266a27110b3317b
Khalifa <alikhalifa1911@gmail.com>
2025-04-27 16:51:23 +0300

    style the cookie banner

--- src/components/cookie-banner/index.tsx
+++ src/components/cookie-banner/index.tsx
@@ -20,30 +20,41 @@ export default function CookieBanner() {
     setShowBanner(false);
   };
 
+  const handleDeny = () => {
+    setShowBanner(false);
+  };
+
   return (
     <div
-      className={`fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 max-w-xl w-full px-6 py-4 bg-white shadow-2xl rounded-2xl border border-gray-200 transition-all duration-500 ease-in-out ${
+      className={`fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 max-w-xl w-full px-6 py-4 bg-indigo-600 shadow-2xl rounded-2xl border border-gray-500 transition-all duration-500 ease-in-out ${
         showBanner
           ? "opacity-100 translate-y-0"
           : "opacity-0 translate-y-10 pointer-events-none"
       }`}
     >
-      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+      <div className="flex flex-col  items-start sm:items-center justify-between gap-4">
         <p
-          className="text-gray-700 text-sm leading-relaxed"
+          className="text-white text-sm leading-relaxed"
           dangerouslySetInnerHTML={{
             __html: t.markup("essential-cookies-accept", {
               important: (chunks) => `<b>${chunks}</b>`,
             }),
           }}
         />
-
-        <button
-          onClick={handleAgree}
-          className="mt-2 sm:mt-0 shrink-0 px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium rounded-xl transition-all duration-300 ease-in-out"
-        >
-          {t("agree")}
-        </button>
+        <div className="flex gap-2 sm:gap-4 mt-4 sm:mt-0">
+          <button
+            onClick={handleAgree}
+            className="mt-2 sm:mt-0 shrink-0 px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white text-sm font-medium rounded-xl transition-all duration-300 ease-in-out"
+          >
+            {t("agree")}
+          </button>
+          <button
+            onClick={handleDeny}
+            className="mt-2 sm:mt-0 shrink-0 px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white text-sm font-medium rounded-xl transition-all duration-300 ease-in-out"
+          >
+            {t("deny")}
+          </button>
+        </div>
       </div>
     </div>
   );
